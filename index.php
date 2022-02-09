e89c2fd7b45d620726f29e70c37bd8561ecb75d6
lenovo ideapad <ideapad.lenovo.s540@gmail.com>
2022-02-09 18:57:55 +0900

    [chore] laravelmix, browsersync add

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-hello
-こんにちは
-<?php
-
-phpinfo();
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>My test page</title>
+  </head>
+  <body>
+      Hello World!
+  </body>
+</html>
