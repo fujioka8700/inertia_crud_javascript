d15927b78f943ce5b46542e534751fe21099fd07
lenovo ideapad <ideapad.lenovo.s540@gmail.com>
2022-02-12 08:59:20 +0900

    [add] textfile.php テキストファイルを読み込む

---
+++ textfile.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>テキストファイルを読み込む</title>
+  </head>
+  <body>
+      <h1>テキストファイルを読み込む</h1>
+      <p><?php readfile("data.txt")?></p>
+  </body>
+</html>
