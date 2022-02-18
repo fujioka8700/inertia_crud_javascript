442e64b5794e30eda3b6e3adf06ee78ef82a1122
lenovo ideapad <ideapad.lenovo.s540@gmail.com>
2022-02-18 17:01:20 +0900

    [add] check.php session3.php include_once関数の使い道

--- session3.php
+++ session3.php
@@ -8,8 +8,7 @@
   <body>
     <h1>ログイン処理</h1>
     <p>[Account: <?php echo htmlspecialchars($account); ?>]</p>
-    <p>新しく用意したページです。</p>
-    <a href="./session3.php">次のページ</a>
+    <p>SESSION3ページです。</p>
     <hr>
   </body>
 </html>
