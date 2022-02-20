3fc6003bb4c82486791a0aa830945facd275dd67
lenovo ideapad <ideapad.lenovo.s540@gmail.com>
2022-02-20 20:08:21 +0900

    [add] text.php テキストファイルの読み込み

---
+++ text.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hello PHP</title>
+  </head>
+  <body>
+      <p><?php $str = readfile("data.txt"); ?></p>
+  </body>
+</html>
